da070a8e59157cd3b68ed49030afae11df86995b
Yadhu <yadhukrishnanas@gmail.com>
2024-04-18 14:14:05 +0530

    aws bucket policy

---
+++ awss3_bucket_policy.js
@@ -0,0 +1,11 @@
+const bucket_policy ={
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::superj/*"
+        }
+    ]
+}
